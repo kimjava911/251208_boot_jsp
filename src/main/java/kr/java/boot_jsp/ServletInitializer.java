5c685411f14518d3dc56590567b75237a19dcaf4
kimjava911 <aibackend4th@gmail.com>
2025-12-08 11:05:33 +0900

    Initial commit

---
+++ src/main/java/kr/java/boot_jsp/ServletInitializer.java
@@ -0,0 +1,13 @@
+package kr.java.boot_jsp;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+}
