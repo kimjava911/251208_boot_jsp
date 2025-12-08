7670b7b974dab217b97a38171125ebcda115b9ec
kimjava911 <aibackend4th@gmail.com>
2025-12-08 12:22:49 +0900

    step3

---
+++ src/main/java/kr/java/boot_jsp/controller/MainController.java
@@ -0,0 +1,31 @@
+package kr.java.boot_jsp.controller;
+
+import kr.java.boot_jsp.service.CustomerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.time.LocalDateTime;
+
+@Controller // Dispacher Servlet에 연결
+@RequiredArgsConstructor
+public class MainController {
+    private final CustomerService customerService;
+
+    @GetMapping("/") // Mapping
+    public String index(Model model) {
+        model.addAttribute("customers",
+                customerService.findAllWithProducts());
+        model.addAttribute("serverTime", LocalDateTime.now());
+        return "index"; // WEB-INF/views/{}.jsp
+    }
+
+    @GetMapping("/customer/{id}") // {변수명}
+    public String customerDetail(@PathVariable Long id, Model model) {
+        model.addAttribute("customer",
+                customerService.findByIdWithProducts(id));
+        return "customer/detail";
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,25 @@
+<%-- 디렉티브 : page --%>
+<%--<%@ page contentType="text/html;charset=UTF-8" language="java" pageEncoding="UTF-8" %>--%>
+<%@ page contentType="text/html;charset=UTF-8"%>
+<%--<%@ page import="" %>--%>
+<%-- 외부 의존성, 타입, 모듈... --%>
+<%@ page import="java.util.List" %>
+<%@ page import="kr.java.boot_jsp.model.entity.Customer" %>
+<%@ page import="java.time.LocalDateTime" %>
+<%@ page import="java.time.format.DateTimeFormatter" %>
+
+<html>
+<head>
+    <title>고객 및 상품 관리</title>
+</head>
+<body>
+    <h1>고객 목록</h1>
+    <%
+        // 일반적인 스크립틀릿 -> 흐름제어, 변수 선언 등
+        List<Customer> customers = (List<Customer>) request.getAttribute("customers");
+        LocalDateTime serverTime = (LocalDateTime) request.getAttribute("serverTime");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    %>
+    <p>서버 시간 : <%= serverTime.format(formatter) %> </p>
+</body>
+</html>
